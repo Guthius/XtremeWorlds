9031e84bfbfb5b5f1ce84417316fd66855568a16
Seth Jones <Hellator@gmail.com>
2025-09-15 01:45:08 -0700

    chore: Luck Stat

--- Source/Server/Database/Script.cs
+++ Source/Server/Database/Script.cs
@@ -970,10 +970,7 @@ public class Script
             var expRollover = GetPlayerExp(index) - GetPlayerNextLevel(index);
             SetPlayerLevel(index, GetPlayerLevel(index) + 1);
             int points = Server.Constant.StatPerLevel;
-            if (GetPlayerStat(index, Stat.Luck) >= 10)
-            {
-                points += ((int)Math.Floor((decimal)GetPlayerStat(index, Stat.Luck) / 10));
-            }
+            points += ((int)Math.Floor((decimal)GetPlayerStat(index, Stat.Luck) / 10));
             SetPlayerPoints(index, GetPlayerPoints(index) + points);
             SetPlayerExp(index, expRollover);
             level_count += 1;
