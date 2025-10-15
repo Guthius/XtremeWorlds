1d5c92159dee23ca40ea3617b31dda11f5f56a8c
Seth Jones <Hellator@gmail.com>
2025-10-14 21:04:38 -0500

    Fix hotbar

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1215,7 +1215,7 @@ namespace Client
                 for (int i = 0; i < Constant.MaxHotbar; i++)
                 {
                     // Check if the corresponding hotbar key is pressed
-                    if (CurrentKeyboardState.IsKeyDown((Keys) ((int) Keys.D1 + i)))
+                    if (CurrentKeyboardState.IsKeyDown((Keys) ((int) Keys.D0 + (i + 1))))
                     {
                         Sender.SendUseHotbarSlot(i);
                         return; // Exit once the matching slot is used
