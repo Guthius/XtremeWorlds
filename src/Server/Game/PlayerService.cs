6be64709647802efd13c3e53d57c3d17168ebf6d
Seth <Hellator@gmail.com>
2026-01-12 14:32:11 -0600

    Revert "Use lock"
    This reverts commit 950d098f52230ffc17db170a87321537eb77906b.

--- src/Server/Game/PlayerService.cs
+++ src/Server/Game/PlayerService.cs
@@ -6,93 +6,53 @@ public sealed class PlayerService : IPlayerService
 {
     public static PlayerService Instance { get; } = new();
 
-    private readonly object _sync = new();
-
     private readonly LinkedList<int> _playerIds = [];
 
     private readonly LinkedList<Player> _players = [];
 
-    // Return snapshots to avoid concurrent-modification exceptions during enumeration.
-    public IEnumerable<Player> Players
-    {
-        get
-        {
-            lock (_sync)
-            {
-                return _players.ToArray();
-            }
-        }
-    }
-
-    public IEnumerable<int> PlayerIds
-    {
-        get
-        {
-            lock (_sync)
-            {
-                return _playerIds.ToArray();
-            }
-        }
-    }
+    public IEnumerable<Player> Players => _players;
+    public IEnumerable<int> PlayerIds => _playerIds;
 
     public bool IsConnected(int playerId)
     {
-        lock (_sync)
-        {
-            return _players.Any(x => x.Id == playerId);
-        }
+        return _players.Any(x => x.Id == playerId);
     }
 
     public void OnAdd(int playerId, INetworkChannel channel)
     {
-        lock (_sync)
-        {
-            _playerIds.AddLast(playerId);
-            _players.AddLast(new Player(playerId, channel));
-        }
+        _playerIds.AddLast(playerId);
+        _players.AddLast(new Player(playerId, channel));
     }
 
     public bool RemovePlayer(int playerId)
     {
-        lock (_sync)
+        var player = _players.FirstOrDefault(x => x.Id == playerId);
+        if (player is null)
         {
-            var player = _players.FirstOrDefault(x => x.Id == playerId);
-            if (player is null)
-            {
-                return false;
-            }
-
-            _playerIds.Remove(playerId);
-            _players.Remove(player);
-
-            return true;
+            return false;
         }
+
+        _playerIds.Remove(playerId);
+        _players.Remove(player);
+
+        return true;
     }
 
     public bool Disconnect(int playerId)
     {
-        lock (_sync)
+        var player = _players.FirstOrDefault(x => x.Id == playerId);
+        if (player is null)
         {
-            var player = _players.FirstOrDefault(x => x.Id == playerId);
-            if (player is null)
-            {
-                return false;
-            }
-
-            player.Disconnect();
-            return true;
+            return false;
         }
+
+        player.Disconnect();
+        return true;
     }
 
     public void SendDataToAll(byte[] bytes)
     {
-        Player[] players;
-        lock (_sync)
-        {
-            players = _players.ToArray();
-        }
-
-        foreach (var player in players)
+        foreach (var player in _players)
         {
             player.Send(bytes);
         }
@@ -100,21 +60,15 @@ public sealed class PlayerService : IPlayerService
 
     public void SendDataTo(int playerId, byte[] bytes)
     {
-        Player player;
-        lock (_sync)
-        {
-            player = _players.FirstOrDefault(x => x.Id == playerId);
-        }
+        var player = _players.FirstOrDefault(x => x.Id == playerId);
 
         player?.Send(bytes);
     }
 
     public string ClientIp(int playerId)
     {
-        lock (_sync)
-        {
-            var player = _players.FirstOrDefault(x => x.Id == playerId);
-            return player is not null ? player.IpAddress : string.Empty;
-        }
+        var player = _players.FirstOrDefault(x => x.Id == playerId);
+
+        return player is not null ? player.IpAddress : string.Empty;
     }
 }
