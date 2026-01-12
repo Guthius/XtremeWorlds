6b0bc499504d1222d35fbe5a57a89a4d362b7046
Seth <Hellator@gmail.com>
2026-01-12 15:00:45 -0600

    Fix account crash

--- src/Server/Game/Globals/Commands.cs
+++ src/Server/Game/Globals/Commands.cs
@@ -4,21 +4,41 @@ namespace Server.Globals
     {
         public static void SetPlayerLogin(int index, string login)
         {
+            if (Account.Instance == null || index < 0 || index >= Account.Instance.Count)
+            {
+                return;
+            }
+
             Account.Instance[index].Login = login;
         }
 
         public static string GetPlayerPassword(int index)
         {
+            if (Account.Instance == null || index < 0 || index >= Account.Instance.Count)
+            {
+                return string.Empty;
+            }
+
             return Account.Instance[index].Password;
         }
 
         public static void SetPlayerPassword(int index, string password)
         {
+            if (Account.Instance == null || index < 0 || index >= Account.Instance.Count)
+            {
+                return;
+            }
+
             Account.Instance[index].Password = password;
         }
 
         public static string GetAccountLogin(int index)
         {
+            if (Account.Instance == null || index < 0 || index >= Account.Instance.Count)
+            {
+                return string.Empty;
+            }
+
             return Account.Instance[index].Login;
         }
     }
